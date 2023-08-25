7f66c309f9571159a8b1afa33295c34cc77c8803
CarlosTolenk <carlos.tolentinoe@gmail.com>
2023-08-25 19:06:14 -0400

    Add Test Container

---
+++ src/modules/shared/__tests__/AxiosClient.test.ts
@@ -0,0 +1,10 @@
+import {AxiosClient} from '../infrastructure/AxiosClient';
+
+describe('AxiosClient', () => {
+  test('should', () => {
+    const httClient: AxiosClient = new AxiosClient();
+
+    expect(httClient.get).toBeDefined();
+    expect(httClient.get()).toBe('AxiosClient');
+  });
+});
