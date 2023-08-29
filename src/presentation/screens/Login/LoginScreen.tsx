d1c26b23de734b127219f38d2ee63f0aa7e44ccf
CarlosTolenk <carlos.tolentinoe@gmail.com>
2023-08-29 12:09:53 -0400

    Implement Formik basic

--- src/presentation/screens/Login/LoginScreen.tsx
+++ src/presentation/screens/Login/LoginScreen.tsx
@@ -1,18 +1,38 @@
 import React from 'react';
-import {Button, Text, View} from 'react-native';
+import {Button, Text, View, TextInput} from 'react-native';
+import {Formik} from 'formik';
 import {useLoginViewModel} from './LoginViewModel';
+
+type ILogin = {
+  email: string;
+  password: string;
+};
 
 const LoginScreen = () => {
   const {login} = useLoginViewModel();
 
-  const onHandlerLogIn = () => {
-    login('carlos', 'password');
+  const onHandlerLogIn = (value: ILogin) => {
+    console.log(value);
+    // login('carlos', 'password');
   };
 
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       <Text>Login</Text>
-      <Button title="LogIn" onPress={onHandlerLogIn} />
+      <Formik
+        initialValues={{email: '', password: ''}}
+        onSubmit={values => onHandlerLogIn(values as ILogin)}>
+        {({handleChange, handleBlur, handleSubmit, values}) => (
+          <View>
+            <TextInput
+              onChangeText={handleChange('email')}
+              onBlur={handleBlur('email')}
+              value={values.email}
+            />
+            <Button onPress={() => handleSubmit()} title="Submit" />
+          </View>
+        )}
+      </Formik>
     </View>
   );
 };
